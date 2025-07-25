b3dfefcbe3d42669cc7607ebe4aec7601d2f2bed
Sambangi Chanti babu <cs22b2037@iiitdm.ac.in>
2025-07-25 17:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from langgraph.graph import StateGraph, END
 from langchain.document_loaders import PyPDFLoader, TextLoader
-from agents.resume_skill_match_agent import resume_skill_match_agent
-from agents.email_agent import email_agent
-from agents.youtube_utility import youtube_utility
+from resume_skill_match_agent import resume_skill_match_agent
+from email_agent import email_agent
+from youtube_utility import youtube_utility
 
 # Define graph state class
 class GraphState(dict): pass
