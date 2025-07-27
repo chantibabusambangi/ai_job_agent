1ced7933b416fb8ae9686353662f802ddf5c8474
Sambangi Chanti babu <cs22b2037@iiitdm.ac.in>
2025-07-27 22:00:08 +0530

    Delete servers directory

--- servers/email_server.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from langchain_mcp_adapters.server import MCPServer
-from email_agent import email_agent_node
-
-app = FastAPI()
-server = MCPServer(app, func=email_agent_node, path="/mcp")
--- servers/youtube_server.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from langchain_mcp_adapters.server import MCPServer
-from resume_score_agent import youtube_utility
-
-app = FastAPI()
-server = MCPServer(app, func=youtube_utility, path="/mcp")
